2218ba996b502018e5ce7a777ab897ed09446327
mobiosoft <polardreammusic@gmail.com>
2023-07-26 22:16:50 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: MyBody(),))
+    )
+);
+
+class Event {
+  final String eventFormat;
+  final String eventDateTime;
+  final String eventTimeZone;
+  final Map<String, dynamic> eventData;
+
+  Event(this.eventFormat, this.eventDateTime, this.eventTimeZone, this.eventData);
+
+  factory Event.fromJSON(dynamic dataJSON) {
+    return Event(
+        dataJSON['EventFormat'] as String,
+        dataJSON['EventDateTime'] as String,
+        dataJSON['EventTimeZone'] as String,
+        dataJSON['EventData'] as Map<String, dynamic>
+    );
+  }
+}
+
+class MyBody extends StatefulWidget {
+  @override
+  createState() => MyBodyState();
+}
+
+class MyBodyState extends State<MyBody> {
+  //List data = [];
+  List<Event> events = [];
+
+  Future<String> getData() async {
+    final response = await http.get(
+        Uri.parse("http://188.225.75.241/getmdata?user=user1@mobiosoft.com"),
+        headers: {
+          "Accept": "application/json"
+        }
+    );
+
+    if ((response.statusCode == 200) || (response.statusCode == 201)) {
+      setState(() {
+        if (response.body != '') {
+          //data = json.decode(response.body);
+          events = (json.decode(response.body) as List).map((i) => Event.fromJSON(i)).toList();
+          //print(events[0].eventData);
+        }
+      });
+      return "Success!";
+    } else {
+        throw Exception('Ошибка загрузки данных');
+    }
+  }
+
+  @override
+  void initState(){
+    getData();
+  }
+
+  //Create item rows for ListView of new events
+  Widget _buildRowSD1(Event item) => Container(
+    decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey))
+    ),
+    margin: const EdgeInsets.fromLTRB(1, 1, 1, 1),
+    child: ListTile(
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(item.eventDateTime.toString(), style: TextStyle(color: Colors.grey[700], fontWeight: FontWeight.bold)),
+          SizedBox(width: 20),
+          Text(item.eventData['name'].toString(), style: TextStyle(color: Colors.blue[500], fontWeight: FontWeight.bold)),
+        ],
+      ),
+      subtitle: Text(item.eventData['content'].toString()),
+      trailing: Text('>'),
+    ),
+  );
+
+  //Create item rows for ListView of new events
+  Widget _buildRowSD2(Event item) => Container(
+    decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey))
+    ),
+    margin: const EdgeInsets.fromLTRB(1, 1, 1, 1),
+    child: ListTile(
+      title: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(item.eventDateTime.toString(), style: TextStyle(color: Colors.grey[700], fontWeight: FontWeight.bold)),
+          SizedBox(width: 20),
+          Text(item.eventData['name'].toString(), style: TextStyle(color: Colors.green[500], fontWeight: FontWeight.bold)),
+        ],
+      ),
+      subtitle: Text(item.eventData['content'].toString()),
+      trailing: Text('>'),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Listviews"), backgroundColor: Colors.blue),
+      body: ListView.builder(
+        itemCount: events == null ? 0 : events.length,
+        itemBuilder: (BuildContext context, int index){
+          /*return Card(
+            child: Text(events[index].eventData.toString()),
+          );*/
+          if (events[index].eventFormat == 'SD1') {
+            return _buildRowSD1(events[index]);
+          } else if (events[index].eventFormat == 'SD2') {
+            return _buildRowSD2(events[index]);
+          }
+          else {
+            return _buildRowSD1(events[index]);
+          }
+        },
+      ),
+    );
+  }
+}
